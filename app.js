96ed9954896255168fde5752610a55d2a0594ff1
Cared24 <ponuszrichard93@gmail.com>
2019-10-31 12:43:26 +0100

    basic weather forecast done

--- app.js
+++ app.js
@@ -1,35 +1,27 @@
-const request = require('request');
+const chalk = require('chalk');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
-const url = 'https://api.darksky.net/forecast/89df8d9c29460ee59f732f16036c2a3c/37.8267,-122.4233?lang=hu';
+// Get Location input
+const location = process.argv[2];
 
-request({ url: url, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to weather service');
-    } else if (response.body.error) {
-        console.log('Unable to find location!');
-    } else {
-        const res = response.body.currently;
-        console.log(response.body.daily.data[0].summary);
-        console.log(`it is currently ${res.temperature} degrees out. There is a ${res.precipProbability}% chance of rain.`);
-    }
+// Calculate input location's forecast
+if (!location) {
+    console.log('Kérjük szépen adjon meg egy egy helyszínt!');
+} else {
+    geocode(location, (error, data) => {
+        console.log(chalk.bgBlue.white('Geolocation'));
+        if (error) {
+            return console.log(error);
+        }
 
-});
-
-// -------------------------------------------------------------
-// Geocoding
-// Address -> Lat/Long -> Weather
-
-const morahalom = 'https://api.mapbox.com/geocoding/v5/mapbox.places/morahalom.json?access_token=pk.eyJ1IjoiY2FyZWQyNCIsImEiOiJjazJhZmo0ZHYxMnRmM2ZvYmN5cmoxcXNxIn0.pDJregwikC4n-jC7RhZ0Mg';
-
-request({ url: morahalom, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to GeoLocation servers!');
-    } else if (response.body.features.length === 0) {
-        console.log('Place cannot be found!');
-    } else {
-        const latitue = response.body.features[0].center[1];
-        const longitude = response.body.features[0].center[0];
-        console.log(`Mórahalom latitude ${latitue}`);
-        console.log(`Mórahalom longitude ${longitude}`);
-    }
-});
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            console.log(chalk.bgGreen.white('Forecast'));
+            if (error) {
+                console.log('Error: ', error);
+            }
+            console.log(data.location);
+            console.log(forecastData);
+        })
+    });
+}
